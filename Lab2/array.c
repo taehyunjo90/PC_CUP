53baedd41fac362a1f7ba8106e76cef99dab6494
Tae Hyun Jo <taehyun.jo.90@gmail.com>
2020-01-14 09:16:06 +0900

    lab2_포맷스타일과 F00_EmptyArray, I00_EmptyArray 해결

--- Lab2/array.c
+++ Lab2/array.c
@@ -1,20 +1,26 @@
 #include "array.h"
 
-int get_index_of(const int numbers[], const size_t element_count, const int num){
+int get_index_of(const int numbers[], const size_t element_count, const int num)
+{
     size_t i;
-    for (i = 0; i < element_count; i++){
-        if (numbers[i] == num){
+    for (i = 0; i < element_count; i++)
+    {
+        if (numbers[i] == num)
+        {
             return i;
         }
     }
     return -1;
 }
 
-int get_last_index_of(const int numbers[], const size_t element_count, const int num){
+int get_last_index_of(const int numbers[], const size_t element_count, const int num)
+{
     size_t i;
     int last_index_of = -1;
-    for (i = 0; i < element_count; i++){
-        if (numbers[i] == num){
+    for (i = 0; i < element_count; i++)
+    {
+        if (numbers[i] == num)
+        {
             last_index_of = i;
         }
     }
@@ -22,16 +28,20 @@ int get_last_index_of(const int numbers[], const size_t element_count, const int
 
 }
 
-int get_max_index(const int numbers[], const size_t element_count){
+int get_max_index(const int numbers[], const size_t element_count)
+{
     size_t i;
     int max_value;
     int max_index = -1;
 
-    if (element_count != 0){
+    if (element_count != 0)
+    {
         max_value = numbers[0];
         max_index = 0;
-        for ( i = 1; i < element_count; i++){
-            if (numbers[i] > max_value){
+        for ( i = 1; i < element_count; i++)
+        {
+            if (numbers[i] > max_value)
+            {
                 max_value = numbers[i];
                 max_index = i;
             }
@@ -40,16 +50,20 @@ int get_max_index(const int numbers[], const size_t element_count){
     return max_index;
 }
 
-int get_min_index(const int numbers[], const size_t element_count){
+int get_min_index(const int numbers[], const size_t element_count)
+{
     size_t i;
     int min_value;
     int min_index = -1;
     
-    if (element_count != 0){
+    if (element_count != 0)
+    {
         min_value = numbers[0];
         min_index = 0;
-        for ( i = 1; i < element_count; i++){
-            if (numbers[i] < min_value){
+        for ( i = 1; i < element_count; i++)
+        {
+            if (numbers[i] < min_value)
+            {
                 min_value = numbers[i];
                 min_index = i;
             }
@@ -58,61 +72,79 @@ int get_min_index(const int numbers[], const size_t element_count){
     return min_index;
 }
 
-int is_all_positive(const int numbers[], const size_t element_count){
+int is_all_positive(const int numbers[], const size_t element_count)
+{
     size_t i;
     
-    for ( i = 0; i < element_count; i++){
-        if (numbers[i] < 0){
+    if (element_count == 0)
+    {
+        return 0;
+    }
+    
+    for ( i = 0; i < element_count; i++)
+    {
+        if (numbers[i] < 0)
+        {
             return 0;
         }
     }
     return 1;
 }
 
-int has_even(const int numbers[], const size_t element_count){
+int has_even(const int numbers[], const size_t element_count)
+{
     size_t i;
     
-    for ( i = 0; i < element_count; i++){
-        if (numbers[i] % 2 == 0){
+    for ( i = 0; i < element_count; i++)
+    {
+        if (numbers[i] % 2 == 0)
+        {
             return 1;
         }
     }
     return 0;
 }
 
-int insert(int numbers[], const size_t element_count, const int num, const size_t pos){
+int insert(int numbers[], const size_t element_count, const int num, const size_t pos)
+{
     size_t i;
     size_t reverse_index;
     
-    if (pos > element_count){
+    if (pos > element_count)
+    {
         return 0;
     }
     
-    for ( i = 0 ; i < element_count + 1; i ++){
+    for ( i = 0 ; i < element_count + 1; i ++)
+    {
         reverse_index = element_count - i;
-        if (reverse_index > pos){
+        if (reverse_index > pos)
+        {
             numbers[reverse_index] = numbers[reverse_index - 1];
-        } else if (reverse_index == pos){
+        } else if (reverse_index == pos)
+        {
             numbers[reverse_index] = num;
         }
     }
     return 1;
-    
 }
 
-int remove_at(int numbers[], const size_t element_count, const size_t index){
+int remove_at(int numbers[], const size_t element_count, const size_t index)
+{
     size_t i;
     
-    if (index > element_count){
+    if (element_count == 0 || index > element_count)
+    {
         return 0;
     }
     
-    for (i = 0; i < element_count; i++){
-        if (i >= index){
+    for (i = 0; i < element_count; i++)
+    {
+        if (i >= index)
+        {
             numbers[i] = numbers[i+1];
         }
     }
     return 1;
-    
 }
 
