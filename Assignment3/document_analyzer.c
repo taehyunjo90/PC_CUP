0a70351c0f7a815fdf654b457e8d91d4454220a4
Tae Hyun Jo <taehyun.jo.90@gmail.com>
2020-03-13 13:05:37 +0900

    위키 테스트케이스 통과 버전

--- Assignment3/document_analyzer.c
+++ Assignment3/document_analyzer.c
@@ -25,7 +25,7 @@ int load_document(const char* document)
     char* p;
 
     FILE* file_stream = fopen(document, "r");
-    
+
     g_document = malloc(sizeof(char***) * 2);
     g_paragraph = malloc(sizeof(char**) * 2);
     g_sentence = malloc(sizeof(char*) * 2);
@@ -34,7 +34,7 @@ int load_document(const char* document)
     if (file_stream == NULL) {
         return  FALSE;
     }
-    
+
     /* load document */
     while (fgets(buffer, BUFFER_LENGTH, file_stream) != NULL) {
         len_readed = strlen(buffer);
@@ -50,8 +50,11 @@ int load_document(const char* document)
     }
     fclose(file_stream);
 
-    if (strcmp(g_loaded_document, "") == 0) {
-        return FALSE;
+    if (g_loaded_document == NULL) {
+        g_loaded_document = malloc(sizeof(char) * 1);
+        g_loaded_document[0] = '\0';
+        g_document = NULL;
+        goto end;
     }
 
     /* Analyze */
@@ -66,7 +69,7 @@ int load_document(const char* document)
 
             g_sentence[cur_word_count++] = pa_word;
             g_sentence = realloc(g_sentence, sizeof(char*) * (cur_word_count + 2));
-            
+
         } else if (is_delim_sentence(p)) {
             set_word_malloc(word_start_ptr, p, &pa_word);
 
@@ -80,11 +83,11 @@ int load_document(const char* document)
 
             g_sentence = malloc(sizeof(char*) * 2);
             cur_word_count = 0;
-            
+
         } else if (is_delim_paragraph(p)) {
             word_start_ptr = get_next_paragraph_start(p);
             p = word_start_ptr;
-            
+
             g_paragraph[cur_sentence_count] = NULL;
             g_document[cur_paragraph_count++] = g_paragraph;
             g_document = realloc(g_document, sizeof(char***) * (cur_paragraph_count + 2));
@@ -100,7 +103,8 @@ int load_document(const char* document)
     g_document[cur_paragraph_count++] = g_paragraph;
     g_document[cur_paragraph_count] = NULL;
 
-
+end:
+    free(g_loaded_document);
     return TRUE;
 }
 
@@ -112,9 +116,13 @@ size_t get_total_word_count(void)
     size_t j = 0;
     size_t k = 0;
 
-    while(g_document[i] != NULL) {
-        while(g_document[i][j] != NULL) {
-            while(g_document[i][j][k] != NULL) {
+    if (g_document == NULL) {
+        return (size_t)0;
+    }
+
+    while (g_document[i] != NULL) {
+        while (g_document[i][j] != NULL) {
+            while (g_document[i][j][k] != NULL) {
                 total_word_count++;
                 k++;
             }
@@ -136,6 +144,10 @@ size_t get_total_sentence_count(void)
     size_t i = 0;
     size_t j = 0;
 
+    if (g_document == NULL) {
+        return (size_t)0;
+    }
+
     while (g_document[i] != NULL) {
         while (g_document[i][j] != NULL) {
             total_sentence_count++;
@@ -152,6 +164,10 @@ size_t get_total_paragraph_count(void)
     size_t total_paragraph_count = 0;
 
     size_t i = 0;
+
+    if (g_document == NULL) {
+        return (size_t)0;
+    }
 
     while (g_document[i] != NULL) {
         total_paragraph_count++;
@@ -245,7 +261,11 @@ int print_as_tree(const char* filename)
     size_t j = 0;
     size_t k = 0;
     FILE* file_stream;
-    
+
+    if (g_document == NULL) {
+        return FALSE;
+    }
+
     file_stream = fopen(filename, "w");
     if (file_stream == NULL) {
         return FALSE;
@@ -259,7 +279,7 @@ int print_as_tree(const char* filename)
             while (g_document[i][j][k] != NULL) {
                 fprintf(file_stream, "    ");
                 fprintf(file_stream, "    ");
-                fprintf(file_stream, "%s", g_document[i][j][k]);    
+                fprintf(file_stream, "%s", g_document[i][j][k]);
                 if (g_document[i + 1] != NULL || g_document[i][j + 1] != NULL || g_document[i][j][k + 1] != NULL) {
                     fprintf(file_stream, "\n");
                 }
@@ -272,7 +292,7 @@ int print_as_tree(const char* filename)
         if (g_document[i + 1] != NULL) {
             fprintf(file_stream, "\n");
         }
-        
+
         i++;
         j = 0;
         k = 0;
@@ -286,7 +306,7 @@ int print_as_tree(const char* filename)
 void set_word_malloc(char* start_word_ptr, char* delim_word_ptr, char** pa_word)
 {
     int word_len = delim_word_ptr - start_word_ptr;
-    
+
     if (word_len > 0) {
         *pa_word = malloc(word_len * sizeof(char) + 1);
         strncpy(*pa_word, start_word_ptr, word_len);
@@ -297,7 +317,7 @@ void set_word_malloc(char* start_word_ptr, char* delim_word_ptr, char** pa_word)
 }
 
 char* get_next_word_start(char* p)
-{   
+{
     char* tmp = p;
 
     while (is_delim_word(tmp) || is_delim_sentence(tmp)) {
@@ -322,43 +342,51 @@ void dispose(void)
     size_t j = 0;
     size_t k = 0;
 
+    if (g_document != NULL) {
         while (g_document[i] != NULL) {
-        while (g_document[i][j] != NULL) {
-            while (g_document[i][j][k] != NULL) {
-                free(g_document[i][j][k]);
-                k++;
+            while (g_document[i][j] != NULL) {
+                while (g_document[i][j][k] != NULL) {
+                    free(g_document[i][j][k]);
+                    k++;
+                }
+                j++;
+                k = 0;
             }
-            j++;
+            i++;
+            j = 0;
             k = 0;
         }
-        i++;
+
+        i = 0;
         j = 0;
         k = 0;
-    }
 
-    i = 0;
-    j = 0;
-    k = 0;
-
-    while (g_document[i] != NULL) {
-        while (g_document[i][j] != NULL) {
-        free(g_document[i][j]);
-        j++;
+        while (g_document[i] != NULL) {
+            while (g_document[i][j] != NULL) {
+                free(g_document[i][j]);
+                j++;
+            }
+            i++;
+            j = 0;
         }
-        i++;
+
+        i = 0;
         j = 0;
+        k = 0;
+
+        while (g_document[i] != NULL) {
+            free(g_document[i]);
+            i++;
+        }
+
+        free(g_document);
     }
 
-    i = 0;
-    j = 0;
-    k = 0;
-
-    while (g_document[i] != NULL) {
-        free(g_document[i]);
-        i++;
-    }
-
-    free(g_document);
+    g_loaded_document = NULL;
+    g_document = NULL;
+    g_paragraph = NULL;
+    g_sentence = NULL;
+    
 
 }
 
@@ -381,7 +409,7 @@ int is_delim_sentence(char* p)
 
 int is_delim_paragraph(char* p)
 {
-    
+
     if (*p == '\n') {
         return TRUE;
     }
--- Assignment3/main.c
+++ Assignment3/main.c
@@ -3,72 +3,96 @@
 #include "document_analyzer.h"
 #include <stdio.h>
 
-int main(void)
+void test_empty(void)
 {
-    const char** sentence = NULL;
-    const char*** paragraph = NULL;
-    size_t i = 0U;
-    size_t j = 0U;
-    
-    assert(load_document("doesntexist.txt") == FALSE);
-    assert(load_document("input.txt") == TRUE);
-    
-    
+    printf("1");
+    assert(load_document("input_empty.txt") == TRUE);
+    printf("2");
+    assert(print_as_tree("output_empty.txt") == FALSE);
+    printf("3");
+    assert(get_total_word_count() == 0U);
+    printf("4");
+    assert(get_total_sentence_count() == 0U);
+    printf("5");
+    assert(get_total_paragraph_count() == 0U);
+    printf("6");
 
-    assert(get_total_word_count() == 58U);
-    assert(get_total_sentence_count() == 9U);
-    assert(get_total_paragraph_count() == 3U);
-
-    assert(get_paragraph(3) == NULL);
-
-    paragraph = get_paragraph(1);
-    assert(paragraph != NULL);
-    assert(get_paragraph_word_count(paragraph) == 14);
-    assert(get_paragraph_sentence_count(paragraph) == 2);
-
-
-    assert(strcmp(paragraph[i][j++], "This") == 0);
-    assert(strcmp(paragraph[i][j++], "is") == 0);
-    assert(strcmp(paragraph[i][j++], "the") == 0);
-    assert(strcmp(paragraph[i][j++], "second") == 0);
-    assert(strcmp(paragraph[i++][j++], "paragraph") == 0);
-
-    j = 0U;
-
-    assert(strcmp(paragraph[i][j++], "I") == 0);
-    assert(strcmp(paragraph[i][j++], "wonder") == 0);
-    assert(strcmp(paragraph[i][j++], "how") == 0);
-    assert(strcmp(paragraph[i][j++], "many") == 0);
-    assert(strcmp(paragraph[i][j++], "sentences") == 0);
-    assert(strcmp(paragraph[i][j++], "you") == 0);
-    assert(strcmp(paragraph[i][j++], "will") == 0);
-    assert(strcmp(paragraph[i][j++], "see") == 0);
-    assert(strcmp(paragraph[i][j++], "here") == 0);
-
-    
-
-    assert(get_sentence(0, 10) == NULL);
-
-    sentence = get_sentence(0, 2);
-    assert(sentence != NULL);
-    assert(get_sentence_word_count(sentence) == 4);
-
-    
-
-    i = 0U;
-
-    assert(strcmp(sentence[i++], "Is") == 0);
-    assert(strcmp(sentence[i++], "this") == 0);
-    assert(strcmp(sentence[i++], "too") == 0);
-    assert(strcmp(sentence[i++], "easy") == 0);
-
-    
-    assert(print_as_tree("output.txt") == TRUE);
-    
 
     dispose();
 
+    puts("test_empty");
+}
+
+void test_document(void)
+{
+    const char** sentence = NULL;
+    const char*** paragraph = NULL;
+    assert(get_paragraph(0) == NULL);
+    assert(get_sentence(0, 0) == NULL);
+    assert(get_total_word_count() == 0U);
+    assert(get_total_sentence_count() == 0U);
+    assert(get_total_paragraph_count() == 0U);
+    assert(print_as_tree("output_document.txt") == FALSE);
+
+    assert(load_document("document.txt") == TRUE);
+
+    assert(get_total_word_count() == 62U);
+    assert(get_total_sentence_count() == 6U);
+    assert(get_total_paragraph_count() == 3U);
+
+    paragraph = get_paragraph(0);
+    assert(paragraph != NULL);
+    assert(get_paragraph_word_count(paragraph) == 19);
+    assert(get_paragraph_sentence_count(paragraph) == 1);
+    sentence = get_sentence(0, 0);
+    assert(sentence != NULL);
+    assert(get_sentence_word_count(sentence) == 19);
+    assert(get_sentence(0, 1) == NULL);
+
+    paragraph = get_paragraph(1);
+    assert(paragraph != NULL);
+    assert(get_paragraph_word_count(paragraph) == 28);
+    assert(get_paragraph_sentence_count(paragraph) == 3);
+    sentence = get_sentence(1, 0);
+    assert(sentence != NULL);
+    assert(get_sentence_word_count(sentence) == 10);
+    sentence = get_sentence(1, 1);
+    assert(sentence != NULL);
+    assert(get_sentence_word_count(sentence) == 9);
+    sentence = get_sentence(1, 2);
+    assert(sentence != NULL);
+    assert(get_sentence_word_count(sentence) == 9);
+    assert(get_sentence(1, 3) == NULL);
+
+    paragraph = get_paragraph(2);
+    assert(paragraph != NULL);
+    assert(get_paragraph_word_count(paragraph) == 15);
+    assert(get_paragraph_sentence_count(paragraph) == 2);
+    sentence = get_sentence(2, 0);
+    assert(sentence != NULL);
+    assert(get_sentence_word_count(sentence) == 8);
+    sentence = get_sentence(2, 1);
+    assert(sentence != NULL);
+    assert(get_sentence_word_count(sentence) == 7);
+    assert(get_sentence(2, 2) == NULL);
+
+    assert(get_paragraph(3) == NULL);
+
+    assert(print_as_tree("output_document.txt") == TRUE);
+
+    dispose();
+
+    puts("test_document");
+}
+
+
+int main(void)
+{
+    test_empty();
+    test_document();
     printf("DONE");
 
     return 0;
 }
+
+
