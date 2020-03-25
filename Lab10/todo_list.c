8db37f280cbaa854542383463cafaf51caee6b37
Tae Hyun Jo <taehyun.jo.90@gmail.com>
2020-03-25 16:04:26 +0900

    lab10 : null exception check.

--- Lab10/todo_list.c
+++ Lab10/todo_list.c
@@ -11,7 +11,7 @@ todo_list_t* init_todo_list_malloc(size_t max_size)
     // init first element
     p->is_meta = true;
     p->total_count = 0;
-    p->max_count = (int32_t) max_size;
+    p->max_count = (int32_t)max_size;
     p->is_empty = false;
 
     tmp_p = p;
@@ -33,18 +33,19 @@ void dispose(todo_list_t* todo_list)
     todo_list_t* p = todo_list;
     int32_t total_count;
     int32_t max_count;
+    
+    if (NULL != p) {
+        total_count = p->total_count;
+        max_count = p->max_count;
 
-    total_count = p->total_count;
-    max_count = p->max_count;
-
-    for (int32_t i = 0; i < max_count; i++) {
-        p++;
-        if (!p->is_empty) {
-            free(p->todo_task);
+        for (int32_t i = 0; i < max_count; i++) {
+            p++;
+            if (!p->is_empty) {
+                free(p->todo_task);
+            }
         }
+        free(todo_list);
     }
-
-    free(todo_list);
 }
 
 bool add_todo(todo_list_t* todo_list, const int32_t priority, const char* task)
@@ -58,7 +59,7 @@ bool add_todo(todo_list_t* todo_list, const int32_t priority, const char* task)
     max_count = p->max_count;
 
     // early exit when is full
-    if (total_count == max_count) {
+    if (total_count == max_count || todo_list == NULL) {
         return false;
     }
 
@@ -78,7 +79,7 @@ bool add_todo(todo_list_t* todo_list, const int32_t priority, const char* task)
 
     todo_list->total_count++;
 
-     return true;
+    return true;
 }
 
 bool complete_todo(todo_list_t* todo_list)
@@ -93,6 +94,10 @@ bool complete_todo(todo_list_t* todo_list)
 
     total_count = p->total_count;
     max_count = p->max_count;
+    
+    if (todo_list == NULL) {
+        return false;
+    }
 
     for (int32_t i = 0; i < max_count; i++) {
         p++;
@@ -107,8 +112,7 @@ bool complete_todo(todo_list_t* todo_list)
 
     if (max_priority_todo == NULL) {
         return false;
-    }
-    else {
+    } else {
         free(max_priority_todo->todo_task);
         max_priority_todo->is_meta = false;
         max_priority_todo->is_empty = true;
@@ -132,6 +136,10 @@ const char* peek_or_null(todo_list_t* todo_list)
 
     total_count = p->total_count;
     max_count = p->max_count;
+    
+    if (todo_list == NULL) {
+        return false;
+    }
 
     for (int32_t i = 0; i < max_count; i++) {
         p++;
@@ -155,14 +163,25 @@ const char* peek_or_null(todo_list_t* todo_list)
 size_t get_count(todo_list_t* todo_list)
 {
     todo_list_t* p = todo_list;
+    
+    if (todo_list == NULL) {
+        return false;
+    }
+    
     return p->total_count;
 }
 
 bool is_empty(todo_list_t* todo_list)
 {
+        
+    if (todo_list == NULL) {
+        return false;
+    }
+    
     if (0 == todo_list->total_count) {
         return true;
     }
+    
     return false;
 }
 
